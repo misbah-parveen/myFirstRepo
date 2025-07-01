7e167a7771ab8346441c453eea2d08c697953207
Misbah Parveen <misbahparveen510@gmail.com>
2025-06-30 22:44:44 -0700

    Add files via upload

---
+++ myfirstprogram.py
@@ -0,0 +1,15 @@
+#This is my first program.
+print("Hello World\nDo you like Mango 🥭?")
+#print sum of two numbers
+num1=45
+num2=50
+sum=num1+num2
+print("sum of two numbers is" ,sum)
+#print num as a string
+a="5"
+b="6"
+print(a+b)
+#print name string
+m="Misbah"
+n="Parveen"
+print(m+n)
